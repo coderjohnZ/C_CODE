57f523fa39ad08e7e96cf4a8c740b906f9bf5ea4
johnZ <loiznioc@qq.com>
2022-03-06 16:28:17 +0800

    学习内容

---
+++ text_3_3/text_3_36/text.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//a
+//'a' - 字符a
+
+//int main()
+//{
+//	char ch = 'a';
+//	//整形
+//	int age = 20;
+//	//单精度浮点型
+//	float weight = 55.5;
+//	double d = 0.0;
+//
+//	return 0;
+//}
+
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("比特\n");
+//	printf("%d\n", 100); //打印一个整数 - %d
+//	//sizeof - 关键字 - 操作符 - 计算类型或者变量所占空间大小
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//创建一个变量
+//	int age = 20;
+//	double weight = 75.3;
+//
+//	age = age + 1;
+//	weight = weight - 20;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//	//%d - 整形；
+//	//%lf - double
+//	//%f - float
+//	return 0;
+//
+//}
+
+//全局变量
+int aab = 100;
+
+//int main()
+//{
+//	//局部变量
+//	int a = 10;
+//	printf("%d\n", a);
+//	//不建议把全局变量和局部变量的名字写成一样的
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	printf("输入两个操作数:>");
+//	scanf("%d %d", &a, & b);//输入函数
+//	sum = a + b;
+//	printf("%d\n", sum);
+//
+//	return 0;
+//
+//}
+
+
